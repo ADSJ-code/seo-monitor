f4c782724dffbffee83dc29f0bd62098f33c0133
André Duarte da Silva Júnior <andreduarteaj@outlook.com>
2025-11-13 02:57:20 -0300

    Force update controllers to fix NameError and ActionNotFound

--- app/controllers/tracked_keywords_controller.rb
+++ app/controllers/tracked_keywords_controller.rb
@@ -1,82 +1,55 @@
+# Force update 20251113
 class TrackedKeywordsController < ApplicationController
-  before_action :set_tracked_keyword, only: %i[ show edit update destroy check_rank ]
+  before_action :set_tracked_keyword, only: %i[ show edit update destroy check_ranking ]
 
-  # GET /tracked_keywords or /tracked_keywords.json
   def index
     @tracked_keywords = TrackedKeyword.all
   end
 
-  # GET /tracked_keywords/1 or /tracked_keywords/1.json
   def show
-    @ranking_histories = @tracked_keyword.ranking_histories.order(checked_on: :desc)
   end
 
-  # GET /tracked_keywords/new
   def new
     @tracked_keyword = TrackedKeyword.new
   end
 
-  # GET /tracked_keywords/1/edit
   def edit
   end
 
-  # POST /tracked_keywords or /tracked_keywords.json
-  # POST /tracked_keywords or /tracked_keywords.json
   def create
     @tracked_keyword = TrackedKeyword.new(tracked_keyword_params)
 
-    respond_to do |format|
-      if @tracked_keyword.save
-        # ## ADICIONE A NOSSA LINHA AQUI ##
-        # Envia o job para o Sidekiq executar em segundo plano
-        SeoCheckWorker.perform_async(@tracked_keyword.id.to_s)
-
-        format.html { redirect_to @tracked_keyword, notice: "Tracked keyword was successfully created. Checking rank in the background." }
-        format.json { render :show, status: :created, location: @tracked_keyword }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @tracked_keyword.errors, status: :unprocessable_entity }
-      end
+    if @tracked_keyword.save
+      redirect_to @tracked_keyword, notice: "Palavra-chave criada. Verificação de ranking em progresso."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /tracked_keywords/1 or /tracked_keywords/1.json
   def update
-    respond_to do |format|
-      if @tracked_keyword.update(tracked_keyword_params)
-        format.html { redirect_to @tracked_keyword, notice: "Tracked keyword was successfully updated." }
-        format.json { render :show, status: :ok, location: @tracked_keyword }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @tracked_keyword.errors, status: :unprocessable_entity }
-      end
+    if @tracked_keyword.update(tracked_keyword_params)
+      redirect_to @tracked_keyword, notice: "Palavra-chave atualizada. Re-verificando o ranking."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /tracked_keywords/1 or /tracked_keywords/1.json
   def destroy
     @tracked_keyword.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to tracked_keywords_path, status: :see_other, notice: "Tracked keyword was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to tracked_keywords_url, notice: "Palavra-chave removida."
   end
 
-  def check_rank
+  def check_ranking
     SeoCheckWorker.perform_async(@tracked_keyword.id.to_s)
-    head :ok
+    redirect_to @tracked_keyword, notice: "Verificação de ranking manual iniciada."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_tracked_keyword
-      @tracked_keyword = TrackedKeyword.find(params.expect(:id))
+      @tracked_keyword = TrackedKeyword.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def tracked_keyword_params
-      # CORREÇÃO: Adicionamos :gl e :hl
-      params.expect(tracked_keyword: [ :keyword, :domain, :gl, :hl ])
+      params.require(:tracked_keyword).permit(:keyword, :domain, :gl, :hl)
     end
 end
