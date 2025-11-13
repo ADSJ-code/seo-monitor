2f4a397066449efbdab733b50a69b0e9d0a0aadc
André Duarte da Silva Júnior <andreduarteaj@outlook.com>
2025-11-13 02:06:02 -0300

    Add callback to clear old history on domain/keyword change

--- app/models/tracked_keyword.rb
+++ app/models/tracked_keyword.rb
@@ -2,7 +2,7 @@ class TrackedKeyword
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  has_many :ranking_histories
+  has_many :ranking_histories, dependent: :destroy
   field :keyword, type: String
   field :domain, type: String
 
@@ -14,4 +14,14 @@ class TrackedKeyword
 
   validates :keyword, presence: true
   validates :domain, presence: true
+
+  before_update :clear_history_if_changed
+
+  private
+
+  def clear_history_if_changed
+    if keyword_changed? || domain_changed?
+      self.ranking_histories.destroy_all
+    end
+  end
 end
