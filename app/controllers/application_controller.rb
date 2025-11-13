91040188462dee86fcec3f389ba9e2f5e263bb7b
André Duarte da Silva Júnior <andreduarteaj@outlook.com>
2025-11-13 02:27:34 -0300

    Trigger worker on update and fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,56 @@
-class ApplicationController < ActionController::Base
-  before_action :set_locale
+class TrackedKeywordsController < ApplicationController
+  before_action :set_tracked_keyword, only: %i[ show edit update destroy check_ranking ]
+
+  def index
+    @tracked_keywords = TrackedKeyword.all
+  end
+
+  def show
+  end
+
+  def new
+    @tracked_keyword = TrackedKeyword.new
+  end
+
+  def edit
+  end
+
+  def create
+    @tracked_keyword = TrackedKeyword.new(tracked_keyword_params)
+
+    if @tracked_keyword.save
+      SeoCheckWorker.perform_async(@tracked_keyword.id.to_s)
+      redirect_to @tracked_keyword, notice: "Palavra-chave criada. Verificação de ranking em progresso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @tracked_keyword.update(tracked_keyword_params)
+      SeoCheckWorker.perform_async(@tracked_keyword.id.to_s)
+      redirect_to @tracked_keyword, notice: "Palavra-chave atualizada. Re-verificando o ranking."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tracked_keyword.destroy!
+    redirect_to tracked_keywords_url, notice: "Palavra-chave removida."
+  end
+
+  def check_ranking
+    SeoCheckWorker.perform_async(@tracked_keyword.id.to_s)
+    redirect_to @tracked_keyword, notice: "Verificação de ranking manual iniciada."
+  end
 
   private
+    def set_tracked_keyword
+      @tracked_keyword = TrackedKeyword.find(params[:id])
+    end
 
-  def set_locale
-    I18n.locale = http_accept_language
-                   .compatible_language_from(I18n.available_locales) || I18n.default_locale
-  end
+    def tracked_keyword_params
+      params.require(:tracked_keyword).permit(:keyword, :domain, :gl, :hl)
+    end
 end
